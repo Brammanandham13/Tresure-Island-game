ac244c78bab17dd1e269297c18da0a999670c020
Brammanandham Anbazhagan <139738006+Brammanandham13@users.noreply.github.com>
2025-04-27 15:03:40 +0530

    Add files via upload

---
+++ Tresure island.py
@@ -0,0 +1,47 @@
+print(r'''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\ ` . "-._ /_______________|_______
+|                   | |o ;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+direction=input("you'r in the road where you want to go?left or right?\n").lower()
+
+if direction=="left":
+    print("you are reached the road end now its lake do u want wait or swim")
+    decsion=input("swim or wait\n").lower()
+    if decsion=="wait":
+        print("now you\'re reached the door! which colour you want choose?")
+        colour_door=input("red or yelow or blue\n").lower()
+        if colour_door=="yellow":
+            print("you win")
+        elif colour_door=="blue":
+            print("Eaten by beasts.Game Over.")
+        elif colour_door=="red":
+            print("Burned by fire.Game Over.")
+        else:
+            print("Game Over")
+    elif decsion=="swim":
+        print("Attacked by trout.Game Over")
+elif direction=="right":
+    print("Fall into a hole.Game Over.")
+
+
+
